8301f255557f7410ecb0667cf0f2c246d8b4c67f
Hardik Mittal <hardiksakshammittal1307@gmail.com>
2025-10-04 19:25:10 +0530

    Added Solution Code of Question 2

---
+++ Part-A_Q.2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    string name;
+    int age;
+    cout<<"Enter Your Name: "<<endl;//Enter Name in Format Hardik_Mittal//
+    cin>>name;
+    cout<<"Enter Your Age: "<<endl;
+    cin>>age;
+    cout<<"Hello "<<name<<", You are "<<age<<" Years Old!"<<endl;
+return 0;
+}
